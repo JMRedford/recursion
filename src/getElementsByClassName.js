3d7c797da5836e7d1d44b66151f463c5812e633c
JMRedford <redford.john.m@gmail.com>
2015-05-28 11:50:53 -0500

    Recursion - John Redford

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var retArray = [];
+  
+  function recurseAddNodes(nodeCollection){
+    for (var i = 0; i < nodeCollection.length; i++){
+      if (nodeCollection[i].nodeType === 1) {
+        if (nodeCollection[i].childNodes.length === 0){
+          if (nodeCollection[i].classList.contains(className)){
+            retArray.push(nodeCollection[i]);
+          }
+        } else { //recursive case
+          if (nodeCollection[i].classList.contains(className)){
+            retArray.push(nodeCollection[i]);
+          }
+          recurseAddNodes(nodeCollection[i].childNodes);
+        }
+      }
+    }
+  }
+
+  recurseAddNodes(document.childNodes);
+
+  return retArray;
 };
